3c655d7ef10c5aefd829bd7c2b8925209c0897f4
alanxavier7777 <alanxavierselbi2028@cs.sjcetpalai.ac.in>
2024-10-01 15:19:42 +0530

    document headder added

--- python1.py
+++ python1.py
@@ -1,3 +1,7 @@
+""""
+Author:Alan xavier selbi
+date:01-10-2024
+python program to get student details"""
 #int("hello python")
 #name = input("enter your name")
 neme = input("enter your name")
@@ -6,4 +10,4 @@ roll_no = int(input("enter your roll number"))
 print("roll number:",roll_no)
 cgp = float(input("enter your cgp"))
 percentage = cgp*10
-print("percantage cgp: ",cgp%10)
+print("percantage cgp: ", cgp%10)
