ceaa2a8cb5568ac036022566606366238aac9b54
alanxavier7777 <alanxavierselbi2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:35 +0530

    Add files via upload
    my first commit

---
+++ python1.py
@@ -0,0 +1,9 @@
+#int("hello python")
+#name = input("enter your name")
+neme = input("enter your name")
+print("name of the student")
+roll_no = int(input("enter your roll number"))
+print("roll number:",roll_no)
+cgp = float(input("enter your cgp"))
+percentage = cgp*10
+print("percantage cgp: ",cgp%10)
